f463cd937d05a7ce580f88260bed9ff60c9b22ae
albert <fengelom@qq.com>
2017-11-26 16:03:14 +0800

    add work list

--- resources/views/la/userinfos/show.blade.php
+++ resources/views/la/userinfos/show.blade.php
@@ -15,17 +15,16 @@
 				<div class="col-md-9">
 					<h4 class="name">{{ $userinfo->$view_col }}</h4>
 					<div class="row stats">
-						<div class="col-md-6 stat"><div class="label2" data-toggle="tooltip" data-placement="top" title="Designation">{{ $userinfo->designation }}</div></div>
-						<div class="col-md-6 stat"><i class="fa fa-map-marker"></i> {{ $userinfo->city or "NA" }}</div>
+						<div class="col-md-6 stat"><div class="label2" data-toggle="tooltip" data-placement="top" title="User Key">{{ $userinfo->user_key }}</div></div>
+						<div class="col-md-6 stat"><i class="fa fa-map-marker"></i> {{ $userinfo->user_name }}</div>
 					</div>
-					<p class="desc">{{ substr($userinfo->about, 0, 33) }}@if(strlen($userinfo->about) > 33)...@endif</p>
 				</div>
 			</div>
 		</div>
 		<div class="col-md-3">
-			<div class="dats1"><i class="fa fa-envelope-o"></i> {{ $userinfo->email }}</div>
-			<div class="dats1"><i class="fa fa-phone"></i> {{ $userinfo->mobile }}</div>
-			<div class="dats1"><i class="fa fa-clock-o"></i> Joined on {{ date("M d, Y", strtotime($userinfo->date_hire)) }}</div>
+			<div class="dats1"><i class="fa fa-envelope-o"></i> {{ $userinfo->password }}</div>
+			<div class="dats1"><i class="fa fa-phone"></i> {{ $userinfo->pay_passwd }}</div>
+			{{--<div class="dats1"><i class="fa fa-clock-o"></i> Joined on {{ date("M d, Y", strtotime($userinfo->date_hire)) }}</div>--}}
 		</div>
 		<div class="col-md-4">
 			<div class="teamview">
@@ -45,7 +44,7 @@
 		</div>
 		<div class="col-md-1 actions">
 			@la_access("UserInfos", "edit")
-				<a href="{{ url(config('laraadmin.adminRoute') . '/userinfos/edit') }}" class="btn btn-xs btn-edit btn-default"><i class="fa fa-pencil"></i></a><br>
+				<a href="{{ url(config('laraadmin.adminRoute') . '/userinfos/'.$userinfo->id.'/edit') }}" class="btn btn-xs btn-edit btn-default"><i class="fa fa-pencil"></i></a><br>
 			@endla_access
 
 			@la_access("UserInfos", "delete")
@@ -73,117 +72,13 @@
 						<h4>General Info</h4>
 					</div>
 					<div class="panel-body">
-						@la_display($module, 'name')
-						@la_display($module, 'designation')
-						@la_display($module, 'gender')
-						@la_display($module, 'mobile')
-						@la_display($module, 'mobile2')
-						@la_display($module, 'email')
-						@la_display($module, 'dept')
-						@la_display($module, 'city')
-						@la_display($module, 'address')
-						@la_display($module, 'about')
-						@la_display($module, 'date_birth')
-						@la_display($module, 'date_hire')
-						@la_display($module, 'date_left')
-						@la_display($module, 'salary_cur')
+						@la_display($module, 'user_key')
+						@la_display($module, 'user_name')
+						@la_display($module, 'password')
+						@la_display($module, 'pay_passwd')
 					</div>
 				</div>
 			</div>
-		</div>
-		<div role="tabpanel" class="tab-pane fade in p20 bg-white" id="tab-timeline">
-			<ul class="timeline timeline-inverse">
-				<!-- timeline time label -->
-				<li class="time-label">
-					<span class="bg-red">
-						10 Feb. 2014
-					</span>
-				</li>
-				<!-- /.timeline-label -->
-				<!-- timeline item -->
-				<li>
-				<i class="fa fa-envelope bg-blue"></i>
-
-				<div class="timeline-item">
-					<span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
-
-					<h3 class="timeline-header"><a href="#">Support Team</a> sent you an email</h3>
-
-					<div class="timeline-body">
-					Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles,
-					weebly ning heekya handango imeem plugg dopplr jibjab, movity
-					jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle
-					quora plaxo ideeli hulu weebly balihoo...
-					</div>
-					<div class="timeline-footer">
-					<a class="btn btn-primary btn-xs">Read more</a>
-					<a class="btn btn-danger btn-xs">Delete</a>
-					</div>
-				</div>
-				</li>
-				<!-- END timeline item -->
-				<!-- timeline item -->
-				<li>
-				<i class="fa fa-user bg-aqua"></i>
-
-				<div class="timeline-item">
-					<span class="time"><i class="fa fa-clock-o"></i> 5 mins ago</span>
-
-					<h3 class="timeline-header no-border"><a href="#">Sarah Young</a> accepted your friend request
-					</h3>
-				</div>
-				</li>
-				<!-- END timeline item -->
-				<!-- timeline item -->
-				<li>
-				<i class="fa fa-comments bg-yellow"></i>
-
-				<div class="timeline-item">
-					<span class="time"><i class="fa fa-clock-o"></i> 27 mins ago</span>
-
-					<h3 class="timeline-header"><a href="#">Jay White</a> commented on your post</h3>
-
-					<div class="timeline-body">
-					Take me to your leader!
-					Switzerland is small and neutral!
-					We are more like Germany, ambitious and misunderstood!
-					</div>
-					<div class="timeline-footer">
-					<a class="btn btn-warning btn-flat btn-xs">View comment</a>
-					</div>
-				</div>
-				</li>
-				<!-- END timeline item -->
-				<!-- timeline time label -->
-				<li class="time-label">
-					<span class="bg-green">
-						3 Jan. 2014
-					</span>
-				</li>
-				<!-- /.timeline-label -->
-				<!-- timeline item -->
-				<li>
-				<i class="fa fa-camera bg-purple"></i>
-
-				<div class="timeline-item">
-					<span class="time"><i class="fa fa-clock-o"></i> 2 days ago</span>
-
-					<h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
-
-					<div class="timeline-body">
-					<img src="http://placehold.it/150x100" alt="..." class="margin">
-					<img src="http://placehold.it/150x100" alt="..." class="margin">
-					<img src="http://placehold.it/150x100" alt="..." class="margin">
-					<img src="http://placehold.it/150x100" alt="..." class="margin">
-					</div>
-				</div>
-				</li>
-				<!-- END timeline item -->
-				<li>
-				<i class="fa fa-clock-o bg-gray"></i>
-				</li>
-			</ul>
-			<!--<div class="text-center p30"><i class="fa fa-list-alt" style="font-size: 100px;"></i> <br> No posts to show</div>-->
 		</div>
 
 	</div>
