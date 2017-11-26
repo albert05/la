9671f732702c0fda18c017f7c5f478a4a43d110f
albert <fengelom@qq.com>
2017-11-26 21:43:45 +0800

    add work config

--- app/Http/Controllers/LA/TasksController.php
+++ app/Http/Controllers/LA/TasksController.php
@@ -108,7 +108,7 @@ class TasksController extends Controller
                 'voucher_id' => $request->voucher_id,
                 'is_kdb_pay' => $request->is_kdb_pay,
                 'prize_number' => $request->prize_number,
-                'run_time' => $request->run_time,
+                'run_time' => date("Y-m-d H:i:s", strtotime($request->run_time)),
                 'status' => 0,
             ]);
 
