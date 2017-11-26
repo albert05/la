afcf358044ca196ec55134086f95e4e91fa0f2fc
albert <fengelom@qq.com>
2017-11-26 14:40:12 +0800

    task

--- resources/views/la/tasks/index.blade.php
+++ resources/views/la/tasks/index.blade.php
@@ -1,84 +1,85 @@
 @extends("la.layouts.app")
 
 @section("contentheader_title", "Tasks")
-@section("contentheader_description", "tasks listing")
+@section("contentheader_description", "Tasks listing")
 @section("section", "Tasks")
 @section("sub_section", "Listing")
-@section("htmlheader_title", "Tasks Listing")
+@section("htmlheader_title", "Task Listing")
 
 @section("headerElems")
-@la_access("Tasks", "create")
-	<button class="btn btn-success btn-sm pull-right" data-toggle="modal" data-target="#AddModal">Add Tasks</button>
-@endla_access
+    @la_access("Tasks", "create")
+    <button class="btn btn-success btn-sm pull-right" data-toggle="modal" data-target="#AddModal">Add Tasks</button>
+    @endla_access
 @endsection
 
 @section("main-content")
 
-@if (count($errors) > 0)
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <div class="box box-success">
+        <!--<div class="box-header"></div>-->
+        <div class="box-body">
+            <table id="example1" class="table table-bordered">
+                <thead>
+                <tr class="success">
+                    @foreach( $listing_cols as $col )
+                        <th>{{ $module->fields[$col]['label'] or ucfirst($col) }}</th>
+                    @endforeach
+                    @if($show_actions)
+                        <th>Actions</th>
+                    @endif
+                </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
     </div>
-@endif
 
-<div class="box box-success">
-	<!--<div class="box-header"></div>-->
-	<div class="box-body">
-		<table id="example1" class="table table-bordered">
-		<thead>
-		<tr class="success">
-			@foreach( $listing_cols as $col )
-			<th>{{ $module->fields[$col]['label'] or ucfirst($col) }}</th>
-			@endforeach
-			@if($show_actions)
-			<th>Actions</th>
-			@endif
-		</tr>
-		</thead>
-		<tbody>
-			
-		</tbody>
-		</table>
-	</div>
-</div>
+    @la_access("Tasks", "create")
+    <div class="modal fade" id="AddModal" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Add Task</h4>
+                </div>
+                {!! Form::open(['action' => 'LA\TasksController@store', 'id' => 'task-add-form']) !!}
+                <div class="modal-body">
+                    <div class="box-body">
+                        @la_form($module)
+                        <div class="form-group">
+                            <label for="role">Role* :</label>
+                            <select class="form-control" required="1" data-placeholder="Select Role" rel="select2" name="role">
+                                <?php $roles = App\Role::all(); ?>
+                                @foreach($roles as $role)
+                                    @if($role->id != 1)
+                                        <option value="{{ $role->id }}">{{ $role->name }}</option>
+                                    @endif
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    {!! Form::submit( 'Submit', ['class'=>'btn btn-success']) !!}
+                </div>
+                {!! Form::close() !!}
+            </div>
+        </div>
+    </div>
+    @endla_access
 
-@la_access("Tasks", "create")
-<div class="modal fade" id="AddModal" role="dialog" aria-labelledby="myModalLabel">
-	<div class="modal-dialog" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<h4 class="modal-title" id="myModalLabel">Add Task</h4>
-			</div>
-			{!! Form::open(['action' => 'LA\TasksController@store', 'id' => 'task-add-form']) !!}
-			<div class="modal-body">
-				<div class="box-body">
-                    @la_form($module)
-					<div class="form-group">
-						<label for="role">Role* :</label>
-						<select class="form-control" required="1" data-placeholder="Select Role" rel="select2" name="role">
-							<?php $roles = App\Role::all(); ?>
-							@foreach($roles as $role)
-								@if($role->id != 1)
-									<option value="{{ $role->id }}">{{ $role->name }}</option>
-								@endif
-							@endforeach
-						</select>
-					</div>
-				</div>
-			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				{!! Form::submit( 'Submit', ['class'=>'btn btn-success']) !!}
-			</div>
-			{!! Form::close() !!}
-		</div>
-	</div>
-</div>
-@endla_access
 
 @endsection
 
@@ -89,23 +90,23 @@
 @push('scripts')
 <script src="{{ asset('la-assets/plugins/datatables/datatables.min.js') }}"></script>
 <script>
-$(function () {
-	$("#example1").DataTable({
-		processing: true,
-        serverSide: true,
-        ajax: "{{ url(config('laraadmin.adminRoute') . '/task_dt_ajax') }}",
-		language: {
-			lengthMenu: "_MENU_",
-			search: "_INPUT_",
-			searchPlaceholder: "Search"
-		},
-		@if($show_actions)
-		columnDefs: [ { orderable: false, targets: [-1] }],
-		@endif
-	});
-	$("#task-add-form").validate({
-		
-	});
-});
+    $(function () {
+        $("#example1").DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: "{{ url(config('laraadmin.adminRoute') . '/task_dt_ajax') }}",
+            language: {
+                lengthMenu: "_MENU_",
+                search: "_INPUT_",
+                searchPlaceholder: "Search"
+            },
+            @if($show_actions)
+            columnDefs: [ { orderable: false, targets: [-1] }],
+            @endif
+        });
+        $("#task-add-form").validate({
+
+        });
+    });
 </script>
 @endpush
