11f4ed012ffcf3c88367dea5d2bf1582734d4810
chenwenfeng <chenwenfeng@wzdai.com>
2017-12-01 16:03:06 +0800

    shake

--- resources/views/la/tasks/edit.blade.php
+++ resources/views/la/tasks/edit.blade.php
@@ -34,8 +34,8 @@
 				@la_input($module, 'title')
 				@la_input($module, 'work_id')
 				@la_input($module, 'user_key')
-				@la_input($module, 'run_time')
-				{{--<div class="form-group"><label for="run_time">Run Time* :</label><div class='input-group datetime'><input id= 'datetimepicker' class="form-control" placeholder="Enter Run Time" required="1" name="run_time" type="text" value="{{$task->run_time}}"></div>--}}
+				{{--@la_input($module, 'run_time')--}}
+				<div class="form-group"><label for="run_time">Run Time* :</label><div class='input-group datetime'><input id= 'datetimepicker' class="form-control" placeholder="Enter Run Time" required="1" name="run_time" type="text" value="{{$task->run_time}}"></div>
 					@la_input($module, 'time_point')
 					@la_input($module, 'product_id')
 					@la_input($module, 'code')
