2271db6382ae0c84aaeb1366477c66d93407326a
albert <fengelom@qq.com>
2017-12-02 14:04:30 +0800

    order sjk

--- resources/views/la/dashboard.blade.php
+++ resources/views/la/dashboard.blade.php
@@ -32,7 +32,6 @@
                 <div class="icon">
                   <i class="ion ion-stats-bars"></i>
                 </div>
-                {{--<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>--}}
               </div>
             </div><!-- ./col -->
             <div class="col-lg-3 col-xs-6">
@@ -45,7 +44,6 @@
                 <div class="icon">
                   <i class="ion ion-person-add"></i>
                 </div>
-                {{--<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>--}}
               </div>
             </div><!-- ./col -->
             <div class="col-lg-3 col-xs-6">
@@ -58,7 +56,6 @@
                 <div class="icon">
                   <i class="ion ion-pie-graph"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
               </div>
             </div><!-- ./col -->
           </div><!-- /.row -->
