b5a120d7c4c50ab4e282d97f1c6f4905dc1da200
albert <fengelom@qq.com>
2017-12-03 16:06:20 +0800

    icon

--- resources/views/la/captchas/index.blade.php
+++ resources/views/la/captchas/index.blade.php
@@ -11,9 +11,9 @@
 		<div class="box-body">
 			<div class="row">
 				<div class="col-md-8 col-md-offset-2">
-					{!! Form::model(null, ['route' => [config('laraadmin.adminRoute') . '.captchas.store'], 'method'=>'PUT', 'id' => 'captcha-edit-form']) !!}
+					{!! Form::model(null, ['route' => [config('laraadmin.adminRoute') . '.captchas.store'], 'method'=>'POST', 'id' => 'captcha-edit-form']) !!}
 					<img src="{{asset('la-assets/img/captcha/captcha.png')}}">
-					<div class="form-group"><label for="run_time">Enter Code* :</label><div class='input-group'> <input class="form-control" placeholder="Enter Code" required="1" name="code" type="text" value=""></div>
+					<div class="form-group"><label for="code">Enter Code* :</label><div class='input-group'> <input class="form-control" placeholder="Enter Code" required="1" name="code" type="text" value=""></div>
 						<br>
 						<div class="form-group">
 							{!! Form::submit( 'Store', ['class'=>'btn btn-success']) !!} <button class="btn btn-default pull-right"><a href="{{ url(config('laraadmin.adminRoute') . '/captchas') }}">Cancel</a></button>
