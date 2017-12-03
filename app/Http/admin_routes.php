71e6c6217edcc67b8167384aef6f1c052635fb43
albert <fengelom@qq.com>
2017-12-03 15:26:12 +0800

    icon

--- app/Http/admin_routes.php
+++ app/Http/admin_routes.php
@@ -89,6 +89,6 @@ Route::group(['as' => $as, 'middleware' => ['auth', 'permission:ADMIN_PANEL']], 
     Route::get(config('laraadmin.adminRoute') . '/workconfig_dt_ajax', 'LA\WorkConfigsController@dtajax');
 
 
-    Route::resource(config('laraadmin.adminRoute') . '/captcha', 'LA\CaptchasController');
+    Route::resource(config('laraadmin.adminRoute') . '/captchas', 'LA\CaptchasController');
 
 });
