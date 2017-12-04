4a0fa360fb9222a086881e81c2a739318855b68f
albert <fengelom@qq.com>
2017-12-04 23:40:39 +0800

    up

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Page / Record not found.</title>
+        <title>503</title>
 
         <link href="https://fonts.googleapis.com/css?family=Roboto:200,400" rel="stylesheet" type="text/css">
-		<link href="{{ asset('la-assets/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
 
         <style>
             html, body {
@@ -47,20 +46,6 @@
     <body>
         <div class="container">
             <div class="content">
-				<i class="fa fa-search" style="font-size:120px;color:#FF5959;margin-bottom:30px;"></i>
-                @if(isset($record_name) && isset($record_id))
-					<div class="title">{{ $record_name }} with id {{ $record_id }} not found</div>
-				@else
-					<div class="title">Page not found</div>
-				@endif
-				
-				@if(Auth::guest())
-					<a href="{{ url('/') }}">Homepage</a> | 
-					<a href="javascript:history.back()">Go Back</a>
-				@else
-					<a href="{{ url(config('laraadmin.adminRoute')) }}">Dashboard.</a> | 
-					<a href="javascript:history.back()">Go Back</a>
-				@endif
             </div>
         </div>
     </body>
