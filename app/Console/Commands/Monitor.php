a48bd5f70524702b395949c198b7074303f29fd6
albert <fengelom@qq.com>
2017-12-10 10:27:09 +0800

    up

--- app/Console/Commands/Monitor.php
+++ app/Console/Commands/Monitor.php
@@ -84,7 +84,7 @@ class Monitor extends Command
         } elseif ($work_id == 'share') {
             DB::table("tasks")->where('id', $id)->update(['run_time' => $this->handleShareTime($time)]);
         } else if ($work_id == 'transfer') {
-            DB::table("tasks")->where('id', $id)->update(['run_time' => date("Y-m-d H:i:s", strtotime($time) + 3600)]);
+            DB::table("tasks")->where('id', $id)->update(['run_time' => $this->handleTransferTime($time)]);
         } else {
             DB::table("tasks")->where('id', $id)->update(['status' => 1]);
         }
@@ -101,6 +101,20 @@ class Monitor extends Command
             $date = date("Y-m-d 17:30:00", time());
         } else {
             $date = date("Y-m-d H:i:s", strtotime($time) + 7200);
+        }
+
+        return $date;
+    }
+
+    private function handleTransferTime($time) {
+        $time = date("Y-m-d H:i:s", max(time(), strtotime($time)));
+
+        $hour = intval(date("H", strtotime($time)));
+
+        if ($hour >= 1 && $hour <= 5) {
+            $date = date("Y-m-d 06:00:00");
+        } else {
+            $date = date("Y-m-d H:i:s", strtotime($time) + 3600);
         }
 
         return $date;
