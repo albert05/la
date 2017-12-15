79cd45f9eaee97611aa28eaff8b6a408a4ba83e0
chenwenfeng <chenwenfeng@wzdai.com>
2017-12-15 17:43:04 +0800

    time point

--- app/Common/Koudai/Base.php
+++ app/Common/Koudai/Base.php
@@ -96,7 +96,7 @@ class Base
         $now = Helper::getMicrotime();
 
         while ($now < $this->time_point) {
-            usleep(1000); // 10毫秒
+            usleep(100); // 0.1毫秒
             $now = Helper::getMicrotime();
         }
 
