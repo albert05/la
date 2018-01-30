154322c5f5a27d8d43342e370856d0ef7dee3f1d
chenwenfeng <chenwenfeng@wzdai.com>
2018-01-30 16:37:49 +0800

    add tool

--- app/Common/Helper.php
+++ app/Common/Helper.php
@@ -181,5 +181,62 @@ class Helper
         return $headerArr;
     }
 
+    /** Json数据格式化
+     * @param  Mixed  $data   数据
+     * @param  String $indent 缩进字符，默认4个空格
+     * @return JSON
+     */
+    public static function jsonFormat($data, $indent=null){
+
+        // 对数组中每个元素递归进行urlencode操作，保护中文字符
+        //array_walk_recursive($data, 'jsonFormatProtect');
+
+        // json encode
+        $data = json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+
+        // 将urlencode的内容进行urldecode
+        $data = urldecode($data);
+
+        // 缩进处理
+        $ret = '';
+        $pos = 0;
+        $length = strlen($data);
+        $indent = isset($indent)? $indent : '&nbsp;&nbsp;&nbsp;&nbsp;';
+        $newline = "\n</br>";
+        $prevchar = '';
+        $outofquotes = true;
+
+        for($i=0; $i<=$length; $i++){
+
+            $char = substr($data, $i, 1);
+
+            if($char=='"' && $prevchar!='\\'){
+                $outofquotes = !$outofquotes;
+            }elseif(($char=='}' || $char==']') && $outofquotes){
+                $ret .= $newline;
+                $pos --;
+                for($j=0; $j<$pos; $j++){
+                    $ret .= $indent;
+                }
+            }
+
+            $ret .= $char;
+
+            if(($char==',' || $char=='{' || $char=='[') && $outofquotes){
+                $ret .= $newline;
+                if($char=='{' || $char=='['){
+                    $pos ++;
+                }
+
+                for($j=0; $j<$pos; $j++){
+                    $ret .= $indent;
+                }
+            }
+
+            $prevchar = $char;
+        }
+
+        return $ret;
+    }
 
 }
--- app/Common/Koudai/Tool.php
+++ app/Common/Koudai/Tool.php
@@ -8,6 +8,7 @@
  */
 namespace App\Common\Koudai;
 
+use App\Common\Helper;
 use \Curl\Curl;
 
 class Tool
@@ -36,66 +37,7 @@ class Tool
         $this->curl->setCookie('SESSIONID', $this->cookie);
         $this->curl->post($this->url, $params);
 
-        return $this->jsonFormat($this->curl->response);
-//        return json_encode($this->curl->response, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
-    }
-
-    /** Json数据格式化
-     * @param  Mixed  $data   数据
-     * @param  String $indent 缩进字符，默认4个空格
-     * @return JSON
-     */
-    public function jsonFormat($data, $indent=null){
-
-        // 对数组中每个元素递归进行urlencode操作，保护中文字符
-        //array_walk_recursive($data, 'jsonFormatProtect');
-
-        // json encode
-        $data = json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
-
-        // 将urlencode的内容进行urldecode
-        $data = urldecode($data);
-
-        // 缩进处理
-        $ret = '';
-        $pos = 0;
-        $length = strlen($data);
-        $indent = isset($indent)? $indent : '&nbsp;&nbsp;&nbsp;&nbsp;';
-        $newline = "\n</br>";
-        $prevchar = '';
-        $outofquotes = true;
-
-        for($i=0; $i<=$length; $i++){
-
-            $char = substr($data, $i, 1);
-
-            if($char=='"' && $prevchar!='\\'){
-                $outofquotes = !$outofquotes;
-            }elseif(($char=='}' || $char==']') && $outofquotes){
-                $ret .= $newline;
-                $pos --;
-                for($j=0; $j<$pos; $j++){
-                    $ret .= $indent;
-                }
-            }
-
-            $ret .= $char;
-
-            if(($char==',' || $char=='{' || $char=='[') && $outofquotes){
-                $ret .= $newline;
-                if($char=='{' || $char=='['){
-                    $pos ++;
-                }
-
-                for($j=0; $j<$pos; $j++){
-                    $ret .= $indent;
-                }
-            }
-
-            $prevchar = $char;
-        }
-
-        return $ret;
+        return Helper::jsonFormat($this->curl->response);
     }
 
     /** 将数组元素进行urlencode
